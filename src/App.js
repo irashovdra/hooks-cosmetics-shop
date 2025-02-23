6b3e9f1dcc466928b3d38b915c4e7e07e51ce350
irashovdra <109846925+irashovdra@users.noreply.github.com>
2025-02-23 14:58:45 -0600

    started perfumes shop

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Perfume } from "./components/Perfume";
+import { PerfumeList } from "./components/PerfumesList";
+
+
+function App() {
+  return (
+    <div className="App">
+      <PerfumeList />
+    </div>
+  );
+}
+
+export default App;
